796f7076e81315dd772b94f132af313eb29bed09
Milkyo <dp221125@naver.com>
2019-10-02 09:56:58 +0900

    Initial Commit

---
+++ CoreDataExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by SeokHo on 2019/10/02.
+//  Copyright © 2019 SeokHo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
